ab39dd5e3c5c62bf960a9e4b48025cf4f05cc177
Jacob Simonson <jsimonson2013@gmail.com>
2018-04-16 16:48:00 -0700

    Check cookie on login page load.

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,11 @@
+window.onload = () => {
+	if (getCookie('UID')) {
+		fetch('http://jacobsimonson.me:3000/bypass/?user=' + getCookie('UID'), {method: 'GET'})
+		.then(res => {return res.json()})
+		.then(res => {window.open(res.url, '__self')})
+	}
+}
+
 const app = new Vue({
 	el: "#form",
 	data: {
@@ -8,12 +16,11 @@ const app = new Vue({
 		login: () => {
 			const payload = `?user=${document.getElementById('user').value}&pass=${document.getElementById('pass').value}`
 			fetch('http://jacobsimonson.me:3000/login/'+payload, {method: 'GET'})
-                        .then(res => {return res.json()})
-                        .then(res => {
-                                console.log(res)
+      .then(res => {return res.json()})
+			.then(res => {
 				document.cookie = 'UID='+res.uid+';path=/'
 				window.open(res.url, '_self')
-                        })
+      })
 		}
 	}
 })
