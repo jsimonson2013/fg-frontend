037ebe5dcee189e72fef91e50ca1dfb4cb1a18f1
jsimonson2013 <jsimonson2013@gmail.com>
2018-05-08 00:39:38 +0000

    Applied view score feature to comments page, too.

--- scripts/comments.js
+++ scripts/comments.js
@@ -1,16 +1,46 @@
 let comments = []
+let promises = []
+let scores = []
 
+const ready = false
+
+const populateComments = local => {
+  for (post of local) {
+    const first = post.author.split(' ')[0]
+    const last = post.author.split(' ')[1]
+
+    const scorePromise = fetch('https://fgapi.jacobsimonson.me/score/?first='+first+'&last='+last, {method: 'GET'})
+    .then(res => {return res.json()})
+    .then(json => {
+      const scr = {
+        author: `${first} ${last}`,
+        score: json.score
+      }
+      scores.push(scr)
+    })
+    promises.push(scorePromise)
+
+  }
+  Promise.all(promises).then(() => {app.ready = true})
+}
 window.onload = () => {
   testLogin()
 
   fetch('https://fgapi.jacobsimonson.me/comments/?parent_id='+getCookie('PID'), {method: 'GET'})
   .then( res => {return res.json()})
-  .then( res => {for(comment of res) comments.push(comment)})
+  .then( res => {
+    for(comment of res) {
+      comments.push(comment)
+    }
+    const copy = comments
+    populateComments(copy)
+  })
 }
 
 const app = new Vue({
   el: '#comment-list',
   data: {
+    ready,
     comments
   },
   methods: {
@@ -19,6 +49,12 @@ const app = new Vue({
     },
     formattedDate: date => {
       return date.substring(0, 10)
+    },
+    getScore: author => {
+      for (score of scores) {
+        if (score.author == author) return score.score
+      }
+      return 0
     }
   }
 })
