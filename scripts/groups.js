5ebb1f15bc8c85a71875564df0b98bde1b9310dc
jsimonson2013 <jsimonson2013@gmail.com>
2018-05-11 12:21:14 +0000

    Group query in group page... need to style.

--- scripts/groups.js
+++ scripts/groups.js
@@ -1,7 +1,17 @@
+let groups = []
+
+window.onload = () => {
+	fetch('https://fgapi.jacobsimonson.me/groups/?user_id='+getCookie('UID'), {method: 'GET'})
+	.then(res => {return res.json()})
+	.then(res => {
+		for (group of res) groups.push(group)
+	})
+}
+
 const app = new Vue({
-  el: '#wrapper',
-  data: {
-    ready: false
-  }
+	el: '#wrapper',
+	data: {
+		groups
+	}
 })
 
