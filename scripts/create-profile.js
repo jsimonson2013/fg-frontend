710dbbc2714a5005a7dc903b02ac0f3a1d0b7f6b
jsimonson2013 <jsimonson2013@gmail.com>
2018-06-28 03:09:39 +0000

    Make user enter code. Autolog them in after signup.

--- scripts/create-profile.js
+++ scripts/create-profile.js
@@ -3,24 +3,20 @@ const app = new Vue({
 	data: {
 		first: "",
 		last: "",
-		email: "",
-		user: "",
+		code: "",
 		pass: ""
 	},
 	methods: {
 		signUp: () => {
-			const payload = `?first=${document.getElementById('first').value}&last=${document.getElementById('last').value}&email=${document.getElementById('email').value}&user=${document.getElementById('user').value}&pass=${document.getElementById('pass').value}`
+			const payload = `?code=${document.getElementById('code').value}&first=${document.getElementById('first').value}&last=${document.getElementById('last').value}&pass=${document.getElementById('pass').value}`
 
 			fetch('https://fgapi.jacobsimonson.me/signup/'+payload, {method: 'GET'})
 			.then(res => {return res.json()})
 			.then(res => {
-				console.log(res)
-				/*
 				document.cookie = 'UID='+res.uid+';path=/;max-age=31536000'
 				document.cookie = 'GID='+res.gid+';path=/;max-age=31536000'
 				document.cookie = 'GNAME='+res.gname+';path=/;max-age=315360000'
 				window.open(res.url, '_self')
-				*/
 			})
 		}
 	}
