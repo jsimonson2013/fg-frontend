186de5baccb45982e8ad71e8d0bf6d8aceaf2dd7
jsimonson2013 <jsimonson2013@gmail.com>
2018-06-30 16:17:44 +0000

    input checking on create

--- scripts/create-profile.js
+++ scripts/create-profile.js
@@ -8,10 +8,26 @@ const app = new Vue({
 	},
 	methods: {
 		signUp: () => {
-			const payload = `?code=${document.getElementById('code').value}&first=${document.getElementById('first').value}&last=${document.getElementById('last').value}&pass=${document.getElementById('pass').value}`
+			const code = document.getElementById('code').value || false
+			const first = document.getElementById('first').value || false
+			const last = document.getElementById('last').value || false
+			const pass = document.getElementById('pass').value || false
+
+			if(!code || !first || !last || !pass) {
+				alert('Please enter the security code from the email, your first name, your lastname, and a password for logging into FriendGroup.')
+				return
+			}
+
+			const payload = `?code=${code}&first=${first}&last=${last}&pass=${pass}`
 
 			fetch('https://fgapi.jacobsimonson.me/signup/'+payload, {method: 'GET'})
-			.then(res => {return res.json()})
+			.then(res => {
+				if (res.status == 404) {
+					alert('Please check your security code and try again.')
+					return
+				}
+				return res.json()
+			})
 			.then(res => {
 				document.cookie = 'UID='+res.uid+';path=/;max-age=31536000'
 				document.cookie = 'GID='+res.gid+';path=/;max-age=31536000'
