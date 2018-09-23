ab6fb83b7ad42ae8add1515784797302d290a02c
jsimonson2013 <jsimonson2013@gmail.com>
2018-09-23 23:04:21 +0000

    Loading message. Fixed populate content bug with comments

--- scripts/comments.js
+++ scripts/comments.js
@@ -13,7 +13,7 @@ window.onload = () => {
 			comments.push(comment)
 		}
 		const copy = comments
-		populateContents('comments', copy, scores, null, null)
+		populateContents('comments', copy, null, scores, null, null, null, getCookie('UNIQ'))
 	})
 }
 
